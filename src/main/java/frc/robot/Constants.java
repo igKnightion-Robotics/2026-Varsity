8418d4eb18defce096df914da0c90fc8516c8c47
JustifiedCrus8r <braalevi@luther.k12.wi.us>
2026-01-31 11:13:12 -0600

    Robot Drives Good

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,7 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kMaxSpeedMetersPerSecond = 1.2; //4.8 before
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     // Chassis configuration
@@ -49,14 +49,14 @@ public final class Constants {
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 10;
+    public static final int kFrontLeftDrivingCanId = 12;
     public static final int kRearLeftDrivingCanId = 14;
-    public static final int kFrontRightDrivingCanId = 12;
+    public static final int kFrontRightDrivingCanId = 10;
     public static final int kRearRightDrivingCanId = 16;
 
-    public static final int kFrontLeftTurningCanId = 11;
+    public static final int kFrontLeftTurningCanId = 13;
     public static final int kRearLeftTurningCanId = 15;
-    public static final int kFrontRightTurningCanId = 13;
+    public static final int kFrontRightTurningCanId = 11;
     public static final int kRearRightTurningCanId = 17;
 
 
